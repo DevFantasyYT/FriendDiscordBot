38d7ddcacf115cf002e249bb385a26b473635a00
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-04-07 17:17:04 +0200

    new command

---
+++ commands/removerole.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, arguments) =>{
+
+        return message.channel.send(`Dit is in aanmaak!`)
+
+}
+
+module.exports.help = {
+    name: "removerole"
+}
